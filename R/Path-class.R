ef3a059a15b53c6585c4c2a6a9cc35312a62e239
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-02-05 13:56:36 +1100

    Add short alias for join

--- R/Path-class.R
+++ R/Path-class.R
@@ -11,6 +11,7 @@ Path <- R6::R6Class(
   public = list(
     initialize = function(path = NULL) private$abstract,
     join = function(other) private$abstact,
+    J    = function(other) self$join(other),
     climb = function(levels) private$abstract,
     print = function() print(self$show)
   ),
@@ -89,6 +90,10 @@ Path$set(
   overwrite = TRUE
 )
 
+# J =====
+#' @describeIn Path$join
+#' @name Path$J
+NULL
 # Active :::::::::::::::::::: ============
 
 # dir ======
